3ca2146624a5f6066c6eae98f8ac97b1401af9f0
kowsar-mahmud-remon <kowsar.remon@gmail.com>
2024-10-10 16:22:30 +0600

    added firebase

---
+++ App/components/firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from Firebase SDKs
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+// Your Firebase configuration (you can use the same config as in your Next.js app)
+const firebaseConfig = {
+  apiKey: "AIzaSyBDi-LwDBDBkCYYd6VXa1o1BrNmx06dg34",
+  authDomain: "nextjs-firebase-chat-app.firebaseapp.com",
+  projectId: "nextjs-firebase-chat-app",
+  storageBucket: "nextjs-firebase-chat-app.appspot.com",
+  messagingSenderId: "38372124480",
+  appId: "1:38372124480:web:857463cf204b298f7a3ca5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+
+export { auth, provider, signInWithPopup, signOut, db };
